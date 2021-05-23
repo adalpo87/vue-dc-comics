2610fed6b3d8cebcfc81b9c55a0afa9141651a1e
adalpo87 <adalpo87@gmail.com>
2021-05-23 17:35:15 +0200

    caricate immagini

---
+++ src/components/MainCard.vue
@@ -0,0 +1,26 @@
+<template>
+  
+    <li>
+        <div class="img-dc">
+            <img :src="hero.thumb" :alt="hero.type">
+        </div>
+        <a href="">{{hero.series}}</a>
+        
+    </li>
+
+</template>
+
+<script>
+export default {
+    name: "MainCard",
+    props: {
+        hero: Object
+    }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/style/vars.scss';
+    @import '../assets/style/main-series.scss';
+</style>
--- src/components/MainDC.vue
+++ src/components/MainDC.vue
@@ -10,70 +10,36 @@
       <div class="series">
           
               <ul>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-                  <li>
-                        <div class="img-dc"></div>
-                        <a href="">Action Comics</a>
-                  </li>
-              
-                  
+                  <MainCard 
+                   v-for="(comic, index) in comics" :key="index"
+                   :hero="comic"
+                   />
               </ul>
             <button>LOAD MORE</button>
       </div>
+      
   </main>
 </template>
 
 <script>
+import comics from '@/assets/data/dc-comics.js'
+import MainCard from '@/components/MainCard.vue'
+
 export default {
     name: 'MainDC',
+    data(){
+          return{
+                comics
+          }
+    },
+    components: {
+          MainCard,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/style/vars.scss';
+    @import '../assets/style/vars.scss';
     @import '../assets/style/main-wrapper.scss';
-    @import '../assets/style/main-series.scss';
+     @import '../assets/style/main-series.scss';
 </style>
